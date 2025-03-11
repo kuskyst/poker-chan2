9eaa9cbe30442d6a587262c3bbefb794abe4102d
kuskyst <126965999+kuskyst@users.noreply.github.com>
2025-03-11 19:00:30 +0900

    Update member-dialog.vue

--- components/member-dialog.vue
+++ components/member-dialog.vue
@@ -1,24 +1,23 @@
 <template>
   <v-dialog v-model="visible" class="w-75">
-    <v-container width="100%" max-width="100%" class="pa-0">
-      <v-table width="100%" max-width="100%" class="pa-4">
-        <thead>
-          <tr>
-            <th class="border-e-sm border-md bg-green-accent-2" colspan="2">members</th>
-          </tr>
-          <tr>
-            <th class="border-e-sm border-md bg-green-accent-1">name</th>
-            <td class="border-md bg-green-accent-1">isVoted</td>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(member, index) in room.members" :key="index">
-            <th class="border-e-sm border-md">{{ member.name }}</th>
-            <td class="border-md">{{ Object.keys(room.votes).includes(member.uuid) ? '○' : '×' }}</td>
-          </tr>
-        </tbody>
-      </v-table>
-    </v-container>
+    <v-table :fixed-header="true" width="100%" max-width="100%" height="500" class="pa-4">
+      <thead>
+        <tr>
+          <th class="border-e-sm border-md bg-green-accent-2" colspan="2">members</th>
+        </tr>
+        <tr>
+          <th class="border-e-sm border-md bg-green-accent-1">name</th>
+          <td class="border-md bg-green-accent-1">isVoted</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(member, index) in room.members" :key="index">
+          <th class="border-e-sm border-md">{{ member.name }}</th>
+          <td class="border-md">{{ Object.keys(room.votes).includes(member.uuid) ? '○' : '×' }}</td>
+        </tr>
+      </tbody>
+    </v-table>
+    <v-btn @click="visible = false" color="blue">close</v-btn>
   </v-dialog>
 </template>
 
